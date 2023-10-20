28204a05dd513e248ad6f78b18e664dde80cb4e0
Tyuiu.ShishkovIA@outlook.com <ilja.shishkov.sidee5@gmail.com>
2023-10-20 22:42:37 +0500

    add Task 0 V 19

---
+++ Tyuiu.ShishkovIA.Sprint3.Task0.V19.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShishkovIA.Sprint3.Task0.V19.Lib;
+
+namespace Tyuiu.ShishkovIA.Sprint3.Task0.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 10;
+            double res = ds.GetSumSeries(startValue, stopValue);
+
+            double wait = 4.399;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShishkovIA.Sprint3.Task0.V19/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShishkovIA.Sprint3.Task0.V19.Lib;
+
+namespace Tyuiu.ShishkovIA.Sprint3.Task0.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Шишков И. А. | АСОиУб-23-2";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #3                                                             *");
+            Console.WriteLine("* Тема: Оператор цикла for                                              *");
+            Console.WriteLine("* Задание #0                                                            *");
+            Console.WriteLine("* Вариант #19                                                           *");
+            Console.WriteLine("* Выполнил: Шишков Илья Александрович | АСОиУб-23-2                     *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                              *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму        *");
+            Console.WriteLine("* ряда по формуле                                                       *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                      *");
+            Console.WriteLine("*************************************************************************");
+            int startValue = 1;
+            int stopValue = 10;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("Сумма радя = " + ds.GetSumSeries(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
