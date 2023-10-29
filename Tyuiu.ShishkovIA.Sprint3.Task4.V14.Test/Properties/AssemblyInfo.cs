3d0a3145464b08f0e63f8b0574d3b14f672aa470
Tyuiu.ShishkovIA@outlook.com <ilja.shishkov.sidee5@gmail.com>
2023-10-29 21:47:12 +0500

    Add Task 4

---
+++ Tyuiu.ShishkovIA.Sprint3.Task4.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShishkovIA.Sprint3.Task4.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShishkovIA.Sprint3.Task4.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e21eaa71-fb69-495f-a7d1-8ace90211d53")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
