4b93533aa0bd2cd090bf8cfb411e3bddbe7d4c4b
Tyuiu.ShishkovIA@outlook.com <ilja.shishkov.sidee5@gmail.com>
2023-10-29 22:33:44 +0500

    Add Task 2 V 20

---
+++ Tyuiu.ShishkovIA.Sprint3.Task2.V20/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShishkovIA.Sprint3.Task2.V20.Lib;
+
+namespace Tyuiu.ShishkovIA.Sprint3.Task2.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Шишков И.А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы цикличекой структуры                                    *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Шишков Илья Александрович | АСОиУб-23-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет         *");
+            Console.WriteLine("* произведение ряда по формуле, при х=1                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int value = 1;
+            int startValue = 1; 
+            int stopValue = 6;
+
+            Console.WriteLine($"Переменная X: {value}");
+            Console.WriteLine($"Старт шага: {startValue}");
+            Console.WriteLine($"Конец шага: {stopValue}");
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write($"Сумма ряда = {dataService.GetMultiplySeries(value, startValue, stopValue)}");
+
+            Console.ReadKey();
+        }
+    }
+}
